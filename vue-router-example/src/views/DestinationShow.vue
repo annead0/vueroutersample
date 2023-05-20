05f4a8ac776216c9e5c6348d970bb528efe4d482
annead0 <120595101+annead0@users.noreply.github.com>
2023-05-20 20:36:09 +0100

    update

--- vue-router-example/src/views/DestinationShow.vue
+++ vue-router-example/src/views/DestinationShow.vue
@@ -1,5 +1,23 @@
 <template>
     <div>
-        <h2>Hello {{$route.params.id}}</h2>
+        <section class="destination">
+        <h2>{{destination.name}}</h2>
+        <div class="destination-details">
+            <img :src="`/images/${destination.image}`" :alt="destination.name"/>
+            <p>{{destination.description}}</p>
+        </div></section>
     </div>
 </template>
+<script>
+import sourceData from '@/data.json'
+export default{
+    computed:{
+        destinationId(){
+         return parseInt(this.$route.params.id)
+        },
+        destination(){
+            return sourceData.destinations.find(destination => destination.id === this.destinationId)
+        }
+    }
+}
+</script>
